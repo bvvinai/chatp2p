6f7114e3011a59a864d47fe729435128c1969adb
bvvinai <venkata.vinai@gmail.com>
2024-05-29 02:01:07 +0530

    init

--- go.mod
+++ go.mod
@@ -7,6 +7,38 @@ toolchain go1.22.3
 require github.com/libp2p/go-libp2p v0.30.0
 
 require (
+	github.com/dgraph-io/badger/v4 v4.2.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/flatbuffers v1.12.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/ipfs/boxo v0.10.0 // indirect
+	github.com/ipfs/go-datastore v0.6.0 // indirect
+	github.com/ipfs/go-log v1.0.5 // indirect
+	github.com/ipld/go-ipld-prime v0.20.0 // indirect
+	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/libp2p/go-libp2p-kbucket v0.6.3 // indirect
+	github.com/libp2p/go-libp2p-record v0.2.0 // indirect
+	github.com/libp2p/go-libp2p-routing-helpers v0.7.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/polydawn/refmt v0.89.0 // indirect
+	github.com/whyrusleeping/go-keyspace v0.0.0-20160322163242-5b898ac5add1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	gonum.org/v1/gonum v0.13.0 // indirect
+)
+
+require (
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -39,6 +71,7 @@ require (
 	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-asn-util v0.3.0 // indirect
 	github.com/libp2p/go-libp2p-core v0.20.1
+	github.com/libp2p/go-libp2p-kad-dht v0.25.2
 	github.com/libp2p/go-msgio v0.3.0 // indirect
 	github.com/libp2p/go-nat v0.2.0 // indirect
 	github.com/libp2p/go-netroute v0.2.1 // indirect
--- main.go
+++ main.go
@@ -6,28 +6,95 @@ import (
 	"os/signal"
 	"syscall"
 
+	badger "github.com/dgraph-io/badger/v4"
 	"github.com/libp2p/go-libp2p"
-	"github.com/multiformats/go-multiaddr"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"golang.org/x/crypto/bcrypt"
 )
 
-const chatProtocolID = "/chat/1.0.0"
+//const chatProtocolID = "/chat/1.0.0"
 
 func main() {
-	node, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"))
-	if err != nil {
-		panic(err)
-	}
-	defer node.Close()
-	fmt.Println(node.ID(), " : ", node.Addrs())
 
-	peerMA, err := multiaddr.NewMultiaddr(node.Addrs()[0].String())
-	if err != nil {
-		panic(err)
+	hostID, err := getUserDetails()
+	if err != nil || hostID == "" {
+		fmt.Println(err)
+		initHost("vinai", "bvvinai")
+	} else {
+		fmt.Printf("PeerID: %s\n", hostID)
 	}
-	fmt.Println(peerMA)
+
+	//initHost("bvvinai", "bvvinai@1357")
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
 	fmt.Println("Received signal, shutting down...")
+}
+
+func getUserDetails() (peer.ID, error) {
+	db, err := badger.Open(badger.DefaultOptions("./badger"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var peerID string
+	get_err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("peerid"))
+		if err != nil {
+			return err
+		}
+		err = item.Value(func(val []byte) error {
+			peerID = string(val)
+			return nil
+		})
+		return err
+	})
+	return peer.ID(peerID), get_err
+}
+
+func initHost(username, password string) {
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	priv, _, err := crypto.GenerateKeyPair(
+		crypto.Ed25519,
+		-1,
+	)
+	if err != nil {
+		panic(err)
+	}
+	privBytes, err := crypto.MarshalPrivateKey(priv)
+	if err != nil {
+		panic(err)
+	}
+
+	host, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"), libp2p.Identity(priv))
+	if err != nil {
+		panic(err)
+	}
+	defer host.Close()
+	fmt.Println(hashedPassword)
+	fmt.Println(priv)
+	fmt.Println(host.ID())
+
+	db, err := badger.Open(badger.DefaultOptions("./badger"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	txn := db.NewTransaction(true)
+	defer txn.Discard()
+
+	txn.Set([]byte("username"), []byte(username))
+	txn.Set([]byte("password"), hashedPassword)
+	txn.Set([]byte("priv"), privBytes)
+	txn.Set([]byte("peerid"), []byte(host.ID()))
+	txn.Commit()
 }
